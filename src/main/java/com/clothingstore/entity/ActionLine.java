092e5f7ac846efce67d397e6bcce723554181275
huynhanh475 <huynhanh475@gmail.com>
2021-12-20 12:30:46 +0700

    Add java class for ActionLine, ImportLine, Order, and OrderLine

---
+++ src/main/java/com/clothingstore/entity/ActionLine.java
@@ -0,0 +1,38 @@
+package com.clothingstore.entity;
+
+public class ActionLine {
+    protected int id;
+    protected int quantity;
+    protected Product code;
+
+    public ActionLine(int id, int quantity, Product code) {
+        this.id = id;
+        this.quantity = quantity;
+        this.code = code;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getCode() {
+        return code;
+    }
+
+    public void setCode(Product code) {
+        this.code = code;
+    }
+    
+}
