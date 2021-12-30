fcb5d4309fc5ab868629d583dba96651fc6cff4e
huynhanh475 <huynhanh475@gmail.com>
2021-12-30 19:05:35 +0700

    Resolve conflicts

--- src/main/java/com/clothingstore/controller/Staffcontroller.java
+++ src/main/java/com/clothingstore/controller/Staffcontroller.java
@@ -5,7 +5,6 @@
  */
 package com.clothingstore.controller;
 
-import java.sql.DriverManager;
 import java.util.List;
 import java.util.ArrayList;
 import java.sql.*;
@@ -13,49 +12,78 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.clothingstore.entity.Staff;
+import com.clothingstore.interfaces.Controller;
 /**
  *
  * @author Avocado
  */
-public class Staffcontroller extends Usercontroller{
-    public static List<Staff> findAll(){
+public class StaffController implements Controller<Staff> {
+    @Override
+    public List<Staff> findAll(){
+        List<Staff> StaffList = new ArrayList<>();
+        
+        Connection connection = null;
+        Statement statement = null;
+        
         try {
-            List<Staff> StaffList = new ArrayList<>();
-            
-            Connection connection = null;
-            Statement statement = null;
-            
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop_test", "root", "Bochan06022001");
-            
+            // try {
+            //     // retrieve the list of staff
+            //     Class.forName("com.mysql.cj.jdbc.Driver");
+            // } catch (ClassNotFoundException ex) {
+            //     Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+            // }
+            // connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop_test", "root", "Bochan06022001");
+            connection = Controller.getConnection();
             // query
             String sql = "select * from staff";
             statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
             
-            while (resultSet.next()){
+            while (resultSet.next()){ 
                 Staff stf;
                 java.sql.Date bd = resultSet.getDate("birthday");
                 java.sql.Date dt = resultSet.getDate("date_started");
                 stf = new Staff(resultSet.getInt("id"),resultSet.getString("full_name"), bd, resultSet.getString("phone"), resultSet.getString("mail"), dt, resultSet.getInt("salary"));
                 StaffList.add(stf);
             }
-            //end connection
-            return StaffList;
         } catch (SQLException ex) {
-            Logger.getLogger(Staffcontroller.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+        } finally{
+            if (statement != null){
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         }
-        return null;
+        //end connection
+        return StaffList;
     }
     
-    public static void insert(Staff stf){
+    @Override
+    public void insert(Staff stf){
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
         try {
-            Connection connection = null;
-            PreparedStatement statement = null;
-            
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop_test", "root", "Bochan06022001");
-            
+            // try {
+            //     // retrieve the list of staff
+            //     Class.forName("com.mysql.cj.jdbc.Driver");
+            // } catch (ClassNotFoundException ex) {
+            //     Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+            // }
+            // connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop_test", "root", "Bochan06022001");
+            connection = Controller.getConnection();
             // query
-            String sql = "insert into Staff(full_name, birthday, phone, mail, date_started, salary) values(?,?,?,?,?,?)";
+            String sql = "insert into staff(full_name, birthday, phone, mail, date_started, salary) values(?,?,?,?,?,?)";
             statement = connection.prepareCall(sql);
             
             statement.setString(1, stf.getFull_name());
@@ -64,63 +92,131 @@ public class Staffcontroller extends Usercontroller{
             statement.setString(4, stf.getMail());
             statement.setDate(5, new java.sql.Date(stf.getDate_started().getTime()));
             statement.setInt(6, stf.getSalary());
-            
+       
             statement.execute();
         } catch (SQLException ex) {
-            Logger.getLogger(Staffcontroller.class.getName()).log(Level.SEVERE, null, ex);
-        }
+            Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+        } finally{
+            if (statement != null){
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        } 
     }
     
-    public static void update(int id, String fullname, Date birthday, String phone, String mail, Date datestarted, int salary){
+    @Override
+    public void update(Staff stf){
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
         try {
-            Connection connection = null;
-            PreparedStatement statement = null;
-            
-            
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop_test", "root", "Bochan06022001");
+            // try {
+            //     // retrieve the list of staff
+            //     Class.forName("com.mysql.cj.jdbc.Driver");
+            // } catch (ClassNotFoundException ex) {
+            //     Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+            // }
+            // connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop_test", "root", "Bochan06022001");
+            connection = Controller.getConnection();
             
             // query
-            String sql = "update Staff set full_name=?, birthday=?, phone=?, mail=?, date_started=?, salary=? where id=?";
+            String sql = "update staff set full_name=?, birthday=?, phone=?, mail=?, date_started=?, salary=?) where id=?";
             statement = connection.prepareCall(sql);
             
-            statement.setString(1, fullname);
-            statement.setDate(2, birthday);
-            statement.setString(3, phone);
-            statement.setString(4, mail);
-            statement.setDate(5, datestarted);
-            statement.setInt(6, salary);
-            statement.setInt(7, id);
+            statement.setString(1, stf.getFull_name());
+            statement.setDate(2, new java.sql.Date(stf.getBirthday().getTime()));
+            statement.setString(3, stf.getPhone());
+            statement.setString(4, stf.getMail());
+            statement.setDate(5, new java.sql.Date(stf.getDate_started().getTime()));
+            statement.setInt(6, stf.getSalary());
+            statement.setInt(7, stf.getId());
             statement.execute();
         } catch (SQLException ex) {
-            Logger.getLogger(Staffcontroller.class.getName()).log(Level.SEVERE, null, ex);
-        }
+            Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+        } finally{
+            if (statement != null){
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        } 
     }
     
-    public static void delete(int id){
+    @Override
+    public void delete(Staff stf){
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
         try {
-            Connection connection = null;
-            PreparedStatement statement = null;
-            
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop_test", "root", "Bochan06022001");
+            // try {
+            //     // retrieve the list of staff
+            //     Class.forName("com.mysql.cj.jdbc.Driver");
+            // } catch (ClassNotFoundException ex) {
+            //     Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+            // }
+            // connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop_test", "root", "Bochan06022001");
+            connection = Controller.getConnection();
             
             // query
-            String sql = "delete from Staff where id=?";
+            String sql = "delete from staff where id=?";
             statement = connection.prepareCall(sql);
-            statement.setInt(1, id);
+            statement.setInt(1, stf.getId());
             statement.execute();
         } catch (SQLException ex) {
-            Logger.getLogger(Staffcontroller.class.getName()).log(Level.SEVERE, null, ex);
-        }
+            Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+        } finally{
+            if (statement != null){
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        } 
     }
     
-    public static List<Staff> findbyid(int id){
+    @Override
+    public List<Staff> findById(int id){
+        List<Staff> StaffList = new ArrayList<>();
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
         try {
-            List<Staff> StaffList = new ArrayList<>();
-            
-            Connection connection = null;
-            PreparedStatement statement = null;
-            
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop_test", "root", "Bochan06022001");
+            // try {
+            //     // retrieve the list of staff
+            //     Class.forName("com.mysql.cj.jdbc.Driver");
+            // } catch (ClassNotFoundException ex) {
+            //     Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+            // }
+            // connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop_test", "root", "Bochan06022001");
+            connection = Controller.getConnection();
             
             // query
             String sql = "select * from staff where id = ?";
@@ -128,18 +224,38 @@ public class Staffcontroller extends Usercontroller{
             statement.setInt(1,id);
             ResultSet resultSet = statement.executeQuery();
             
-            while (resultSet.next()){
+            while (resultSet.next()){ 
                 Staff stf;
                 java.sql.Date bd = resultSet.getDate("birthday");
                 java.sql.Date dt = resultSet.getDate("date_started");
                 stf = new Staff(resultSet.getInt("id"),resultSet.getString("full_name"), bd, resultSet.getString("phone"), resultSet.getString("mail"), dt, resultSet.getInt("salary"));
                 StaffList.add(stf);
             }
-            //end connection
-            return StaffList;
         } catch (SQLException ex) {
-            Logger.getLogger(Staffcontroller.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+        } finally{
+            if (statement != null){
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(StaffController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         }
+        //end connection
+        return StaffList;
+    }
+
+    @Override
+    public List<Staff> findById(String code) {
+        // This doesn't do anything as the staff id is not a string
         return null;
     }
 }
